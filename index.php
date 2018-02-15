6cb5a7a21604dec1d6f2e5530cee538d63ee548a
Ricardo Hdz <rick.mc.fly2@gmail.com>
2018-02-15 08:42:02 -0600

    ADD: Archivo index.php y registro.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang = "es">
+	<head>
+		<meta charset = "utf-8"/>
+		<meta name = "viewport" content = "width=device-width"/>
+	</head>
+
+	<body>
+		<h2>Bienvenida</h2>
+		</br>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet est nulla, semper tincidunt elit viverra in. In ut ultrices tortor. Donec iaculis lectus sodales, molestie enim a, posuere augue.</p>
+		</br>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet est nulla, semper tincidunt elit viverra in. In ut ultrices tortor. Donec iaculis lectus sodales, molestie enim a, posuere augue.</p>
+		</br>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet est nulla, semper tincidunt elit viverra in. In ut ultrices tortor. Donec iaculis lectus sodales, molestie enim a, posuere augue.</p>
+		</br>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin imperdiet est nulla, semper tincidunt elit viverra in. In ut ultrices tortor. Donec iaculis lectus sodales, molestie enim a, posuere augue.</p>
+
+		<a href = "registro.php">Registro</a>
+	</body>
+</html>		
---
+++ registro.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset = "utf-8"/>
+		<meta name = "viewport" content = "width=device-width"/>
+	</head>
+
+	<body>
+		<h2>Registro de usuarios</h2>
+		</br>
+		<p>El siguiente formulario sirve para registrarse en el sistema</p>
+		</br>
+		<form method = "post">
+			<legend>Formulario de registro</legend>
+			<fieldset>
+				<label for = "nombre">Nombre: </label>
+				<input name = "nombre" type = "text"/>
+				</br>
+				<label for = "apellidoP">Apellido paterno: </label>
+				<input name = "apellidoP" type = "text"/>
+				</br>
+				<label for = "apellidoM">Apellido materno: </label>
+				<input name = "apellidoM" type = "text"/>
+				</br>
+				<label for = "telefono">Tel&eacute;fono: </label>
+				<input name = "telefono" type = "text"/>
+				</br>
+				<label for = "contra">Contrase&ntilde;a: </label>
+				<input name = "contra" type = "password"/>
+			</fieldset>
+		</form>	
+	</body>
+</html>
